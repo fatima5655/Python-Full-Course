620346ec3db21ab0fca83827221cc703119408c2
Phool Fatima <104932589+fatima5655@users.noreply.github.com>
2023-10-09 12:49:33 +0500

    Add files via upload

---
+++ first program (1).py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World")
+
+
+# In[2]:
+
+
+print(78)
+
+
+# In[3]:
+
+
+print(7+8)
+
+
+# In[4]:
+
+
+print(2*2)
+
+
+# In[5]:
+
+
+print(12/3)
+
+
+# In[6]:
+
+
+print(9-3)
+
+
+# In[ ]:
+
+
+
+
