955d74cefad26adf06a80ac98854409134093f49
Phool Fatima <104932589+fatima5655@users.noreply.github.com>
2023-10-10 13:17:44 +0500

    Add files via upload

---
+++ Datatype and Variables in Python.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+x = 5 
+print(x)
+
+
+# In[4]:
+
+
+y = 3.14 
+print(y)
+
+
+# In[5]:
+
+
+name = "Alice"  
+print(name)
+
+
+# In[6]:
+
+
+is_valid = True 
+print(is_valid)
+
+
+# In[7]:
+
+
+my_list = [1, 2, 3]  
+print(my_list)
+
+
+# In[8]:
+
+
+my_dict = {'key': 'value'} 
+print(my_dict)
+
+
+# In[ ]:
+
+
+
+
