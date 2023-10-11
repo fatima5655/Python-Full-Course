524236cdb96c4859c18b6c3c1c895aaf3c87bcda
Phool Fatima <104932589+fatima5655@users.noreply.github.com>
2023-10-11 09:20:15 +0500

    Add files via upload

---
+++ user input in python.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+x = input("Enter your first number:")
+y= input("Enter your second number:")
+print(x+y)
+
+
+# In[4]:
+
+
+x = input("Enter your first number:")
+y= input("Enter your second number:")
+print(int(x) +int(y))
+
+
+# In[ ]:
+
+
+
+
